20f4cfdd5ab76fc477a2260fe0e2a08e043cc465
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2026-01-07 16:54:47 -0300

    SlideBar da Home está pronto e funcionando

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 // import { useNavigate } from "react-router-dom";
 
-import Style from "./home.module.css";
+// import Style from "./home.module.css";
 
 // Import de Componentes
 import SlideBar from "../layout/slide-bar/SlideBar";
-import TopBar from "../layout/top-bar/TopBar";
+// import TopBar from "../layout/top-bar/TopBar";
 
 export default function TelaHome(){
     // const navigate = useNavigate();
     
     return(
         <div>
-            <SlideBar />
+            <SlideBar corFundo={"Home"} />
             {/* <TopBar /> */}
         </div>
     )
