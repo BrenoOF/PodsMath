d69d481e26c20a9b1c4e2dcdfa6b643fbd923498
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2026-01-12 16:23:42 -0300

    Primeiro Bloco da tela Home Pronto

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -7,8 +7,28 @@ import Style from "./home.module.css";
 export default function TelaHome() {
     return (
         <div className={Style.containerHome}>
-            <h1>Home</h1>
-            <p>Conteúdo da Home</p>
+            <div className={Style.divBoasVindas}>
+                <div className={Style.pequenaMensagem}>
+                    <i className="fa-regular fa-star" style={{ color: "#ff5724" }}></i>
+                    <p>Plataforma Educacional</p>
+                </div>
+                <h1 className={Style.tituloBoasVindas}>
+                    <span>
+                        Aprenda Matemática
+                    </span>
+                    <br />
+                    quando e onde quiser
+                </h1>    
+                <p className={Style.subTitulo}>
+                    Transforme seu aprendizado com podcasts educacionais de matemática.
+                    <br />
+                    Conteúdo de qualidade para estudantes e professores.
+                </p>
+                <img src={require("../../imgs/microfone.png")} alt="imagem de um microfone"
+                    className={Style.imgMicrofone}
+                    draggable="false"
+                />
+            </div>
         </div>
     )
 }
