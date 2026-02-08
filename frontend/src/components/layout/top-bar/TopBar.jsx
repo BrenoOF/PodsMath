d7fc21548d1b84ae0be01f4002c8cc24fdb251e7
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2026-02-08 19:19:37 -0300

    Correção do TOAST do Prime React pois não estava ficando com estilo/css

--- frontend/src/components/layout/top-bar/TopBar.jsx
+++ frontend/src/components/layout/top-bar/TopBar.jsx
@@ -59,7 +59,7 @@ export default function CompTopBar({ slidebarAberta }) {
         }));
 
         toast.current.show({
-            severity: 'contrast',
+            severity: 'info',
             summary: 'Tema alterado',
             detail: modo === 'dark'
                 ? 'Modo escuro ativado'
@@ -86,7 +86,7 @@ export default function CompTopBar({ slidebarAberta }) {
         }));
 
         toast.current.show({
-            severity: 'contrast',
+            severity: 'info',
             summary: 'Tamanho da Fonte Alterado',
             detail:
                 tamanho === "normal" ? "Fonte normal ativada" :
