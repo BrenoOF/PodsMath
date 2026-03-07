03095972b7b369999e787283934b77230dea3d01
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2026-03-06 21:48:18 -0300

    Tela de Histórico feita + responsividade em diversas partes

--- frontend/src/components/favoritos/Favoritos.jsx
+++ frontend/src/components/favoritos/Favoritos.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Style from "./favoritos.module.css";
+import StyleExterno from "../home/carrosseis/carrosseis.module.css";
 
 export default function TelaFavoritos() {
     const [podcastsFavoritos, setPodcastsFavoritos] = useState([]);
@@ -50,16 +51,16 @@ export default function TelaFavoritos() {
             </div>
             <div className={Style.divCardNovidade}>
                 {podcastsFavoritos.map(item => (
-                    <div className={Style.cardNovidade} key={item.id}>
-                        <div className={Style.divImgCardNovidade}>
+                    <div className={StyleExterno.cardNovidade} key={item.id}>
+                        <div className={StyleExterno.divImgCardNovidade}>
                             <img src={item.img} alt={item.titulo}
-                                className={Style.imgCard} draggable="false"
+                                className={StyleExterno.imgCard} draggable="false"
                             />
                         </div>
                         <h1>{item.titulo}</h1>
                         <p>{item.descricao}</p>
                         <div className={Style.divAcoesBottom}>
-                            <div className={Style.btnAssunto}>
+                            <div className={StyleExterno.btnAssunto}>
                                 <p>{item.assunto}</p>
                             </div>
                             <div className={`
